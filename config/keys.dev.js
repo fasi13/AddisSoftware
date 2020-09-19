d54b38a2899094c2763387add219e4dff32d863a
Cassiopeia-thech <57096209+Cassiopeia-thech@users.noreply.github.com>
2020-09-19 09:43:36 +0300

    Addis App finalize

--- config/keys.dev.js
+++ config/keys.dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://fasile:3r7ELZer8CJrSJmS@cluster0.ws4v3.mongodb.net/addis?retryWrites=true&w=majority",
-    secretOrKey: "FxUum76z"
+    mongoURI: "mongodb://localhost:27017/addis",
+    secretOrKey: "secreate"
 };
 
